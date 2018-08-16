52e88a0d31b7a6e236849f8d61ac4cfa45f87428
NikolasMelui <slak@samaradom.ru>
2018-08-16 18:10:42 +0400

    Update htaccess.sh

--- htaccess.sh
+++ htaccess.sh
@@ -1,4 +1,4 @@
-cp ./_htaccess/base.htaccess > ./.htaccess
-cp ./_htaccess/core.htaccess > ./core/.htaccess
-cp ./_htaccess/manager/core.htaccess > ./manager/.htaccess
-cp ./_htaccess/connectors/manager.htaccess > ./connectors/.htaccess
+cp ./_htaccess/base.htaccess ./.htaccess
+cp ./_htaccess/core.htaccess ./core/.htaccess
+cp ./_htaccess/manager.htaccess ./manager/.htaccess
+cp ./_htaccess/connectors.htaccess ./connectors/.htaccess
