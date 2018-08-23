11f07f2d57dd8cec35df20370f680d5d71f2c43d
NikolasMelui <slak@samaradom.ru>
2018-08-24 00:31:08 +0400

    Add new gulptasks for new package.json file and new gulp module (deleted 4 and add #4 from repo)

--- gulptasks/sass.gulptask.js
+++ gulptasks/sass.gulptask.js
@@ -4,7 +4,7 @@ const $ = require('gulp-load-plugins')({
 	},
 });
 
-const gulp = require('gulp4'),
+const gulp = require('gulp'),
 	combiner = require('stream-combiner2').obj;
 
 const isDevelopment = process.env.NODE_ENV == 'development';
@@ -14,12 +14,24 @@ module.exports = options => {
 		return combiner(
 			gulp.src(options.src),
 			$.debug({ title: 'Get SASS src:' }),
-			$.if(isDevelopment, combiner($.sourcemaps.init(), $.debug({ title: 'Init SASS sourcemaps:' }))),
+			$.if(
+				isDevelopment,
+				combiner(
+					$.sourcemaps.init(),
+					$.debug({ title: 'Init SASS sourcemaps:' })
+				)
+			),
 			$.sass({ outputStyle: 'expand' }),
 			$.rename({ suffix: '.min', prefix: '' }),
 			$.autoprefixer(['last 15 versions']),
 			$.if(!isDevelopment, $.cleancss()),
-			$.if(isDevelopment, combiner($.sourcemaps.write(), $.debug({ title: 'Write SASS sourcemaps:' }))),
+			$.if(
+				isDevelopment,
+				combiner(
+					$.sourcemaps.write(),
+					$.debug({ title: 'Write SASS sourcemaps:' })
+				)
+			),
 			gulp.dest('assets/css'),
 			$.debug({ title: 'Wtite CSS to dest:' })
 		).on('error', error => console.log(error));
